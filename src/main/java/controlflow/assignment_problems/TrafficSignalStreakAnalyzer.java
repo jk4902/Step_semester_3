76162d7d3c4817b081e6b24e0940e8df3679ae2a
Kalavagunta Jayasri <jk490@srmist.edu.in>
2026-09-19 00:01:25 +0530

    Add Week 1 assignment problems

---
+++ src/main/java/controlflow/assignment_problems/TrafficSignalStreakAnalyzer.java
@@ -0,0 +1,49 @@
+package controlflow.assignment_problems;
+
+public class TrafficSignalStreakAnalyzer {
+
+    static void findLongestStreak(String signalLog) {
+
+        if (signalLog == null || signalLog.length() == 0) {
+            System.out.println("No Signal Readings Found");
+            return;
+        }
+
+        char longestColor = signalLog.charAt(0);
+        int longestStreak = 1;
+
+        char currentColor = signalLog.charAt(0);
+        int currentStreak = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == currentColor) {
+
+                currentStreak++;
+
+            } else {
+
+                currentColor = signalLog.charAt(i);
+                currentStreak = 1;
+            }
+
+            if (currentStreak > longestStreak) {
+
+                longestStreak = currentStreak;
+                longestColor = currentColor;
+            }
+        }
+
+        System.out.println(
+                "Longest Streak: '" + longestColor +
+                "' repeated " + longestStreak + " times"
+        );
+    }
+
+    public static void main(String[] args) {
+
+        String signalLog = "RRGGGYRR";
+
+        findLongestStreak(signalLog);
+    }
+}
